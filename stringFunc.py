df3700da7fdcecab47e9107d37ae6d80dde2288d
D. T. Nguyen <idahousa@users.noreply.github.com>
2018-03-16 18:49:40 +0900

    Update stringFunc.py

--- stringFunc.py
+++ stringFunc.py
@@ -5,7 +5,7 @@ import os
 #Descriptions#
 #Extract the extern of a file name.
 #For example, file_name = 'abc/xyz.txt' => should return 'txt'
-def file_extern(file_name):
+def get_file_extern(file_name):
     file_len = len(file_name)
     index = 0
     for i in range(1,file_len):
@@ -25,6 +25,40 @@ def file_extern(file_name):
 def list_files_in_directory(directory_path):
     return os.listdir(directory_path)
 #=============================================================#
-#Modified: 2018-xx-xx
+#Modified: 2018-03-16
 #Author: datnt
 #Descriptions#
+#Make a list of all image files in a given directory#
+def list_image_files(directory_path):
+    rtn_list = []
+    file_extern_list = ['bmp','BMP','jpg','JPG','jpeg','JPEG','tiff','TIFF','PNG','png','gif','GIF']
+    all_file_list = list_files_in_directory(directory_path)
+    for file_name in all_file_list:
+        file_extern = get_file_extern(file_name)
+        if file_extern in file_extern_list:
+            rtn_list.append(file_name)
+            
+    return rtn_list
+#=============================================================#
+#Modified: 2018-03-16
+#Author: datnt
+#Descriptions#
+#Make a list of all image files in a given directory#
+#The extern of image file is "file_extern".
+#Example: list_image_files_with_given_extern('data','jpg') => list all jpeg file in 'data' directory.
+def list_image_files_with_given_extern(directory_path, file_extern):
+    rtn_list = []
+    file_extern_list = [file_extern]
+    all_file_list = list_files_in_directory(directory_path)
+    for file_name in all_file_list:
+        file_extern = get_file_extern(file_name)
+        if file_extern in file_extern_list:
+            rtn_list.append(file_name)
+    return rtn_list
+#=============================================================#
+#Modified: 2018-03-16
+#Author: datnt
+#Descriptions#
+    
+    
+    
