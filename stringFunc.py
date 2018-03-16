d70b80224d07f524cbee49cb3a04e2623634eacd
idahousa <idahousa@users.noreply.github.com>
2018-03-16 18:14:13 +0900

    Add files via upload

---
+++ stringFunc.py
@@ -0,0 +1,13 @@
+#Extract the extern of a file name.
+#For example, abc/xyz.txt => should return 'txt'
+def file_extern(file_name):
+    file_len = len(file_name)
+    index = 0
+    for i in range(1,file_len):
+        if(file_name[-i]=='.'):
+            index = -i+1
+            break
+    if index ==0:
+        return 'error'
+    else:
+        return file_name[index:file_len]
