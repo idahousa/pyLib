58c55324ef5e8b8d739de110b354b3db190ff031
D. T. Nguyen <idahousa@users.noreply.github.com>
2018-03-20 16:08:10 +0900

    Update dataminingLib.py

--- dataminingLib.py
+++ dataminingLib.py
@@ -1,7 +1,23 @@
 import sklearn
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 #=========================================================================================================================================#
 #Date-Of-Code: 2018 - xx - xx
 #Author:
 #Descriptions:
-#
+#Perform a principal component analysis on input data
+#=> Find the new coordinate system by which the data are best represented by maximizing the variance of each data classes in the new system
 #=========================================================================================================================================#
+def perform_pca(data, num_components):
+  
+
+  
+
+#=========================================================================================================================================#
+#Date-Of-Code: 2018 - xx - xx
+#Author:
+#Descriptions:
+#Perform a linear discriminant analysis on input data
+#=> Find the new coordinate system by which the data are best represented and the data of classeses are largestly separated
+#=========================================================================================================================================#
+def perform_ldb(data, groundtruth_label, num_components):
