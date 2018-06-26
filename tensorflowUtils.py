9f49aa18303dccbd48b7fb2be388320376e030da
D. T. Nguyen <idahousa@users.noreply.github.com>
2018-06-26 17:24:31 +0900

    Update tensorflowUtils.py

--- tensorflowUtils.py
+++ tensorflowUtils.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import stringUtils
+import os
 #=============================================================#
 # Modified: 2018-06-26
 # Author: datnt
@@ -9,7 +10,7 @@ import stringUtils
 # return a TENSOR of image data.
 def read_image_data(path_to_image_file):
     contents = tf.read_file(path_to_image_file)
-    file_extern = get_file_extern(path_to_image_file)
+    file_extern = stringUtils.get_file_extern(path_to_image_file)
     if file_extern == 'jpg':
         return tf.image.decode_jpeg(contents=contents)
     elif file_extern == 'png':
@@ -27,7 +28,7 @@ def read_image_data(path_to_image_file):
 # read all image in a directory specified by path_to_dataset
 # return a list of image data ndarray.
 def read_image_dataset(path_to_dataset):
-    image_list = list_image_files(path_to_dataset)
+    image_list = stringUtils.list_image_files(path_to_dataset)
     num_image = len(image_list)
     if num_image >0:
         dataset = []
